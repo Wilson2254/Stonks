f068db599d86a8392dd07f5980c9161fde1b6877
Alexander_Poteryayko <sanek191296@mail.ru>
2020-11-18 19:53:43 +0300

    deploy

---
+++ dist/precache-manifest.090523da0c3ecf1381c4ccfdf19bbda5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfae24a32a31b8c058e3",
+    "url": "/stonks/css/app.d397b87c.css"
+  },
+  {
+    "revision": "7612a9049a300b3fec01",
+    "url": "/stonks/css/chunk-e5a2c086.f367418e.css"
+  },
+  {
+    "revision": "58e74857539ffc747669",
+    "url": "/stonks/css/chunk-eec10044.7d3167a7.css"
+  },
+  {
+    "revision": "70c96a18a06e7ea901128262de529c70",
+    "url": "/stonks/img/logo.70c96a18.png"
+  },
+  {
+    "revision": "d4cfcc625684ee9e81169ae95625046c",
+    "url": "/stonks/index.html"
+  },
+  {
+    "revision": "bfae24a32a31b8c058e3",
+    "url": "/stonks/js/app.975d053a.js"
+  },
+  {
+    "revision": "7612a9049a300b3fec01",
+    "url": "/stonks/js/chunk-e5a2c086.ae4d0a5a.js"
+  },
+  {
+    "revision": "58e74857539ffc747669",
+    "url": "/stonks/js/chunk-eec10044.be977d66.js"
+  },
+  {
+    "revision": "4cc993368947d9c8dd53",
+    "url": "/stonks/js/chunk-vendors.81fb0fc0.js"
+  },
+  {
+    "revision": "d6881c495980b6727d4f3e8d816ad163",
+    "url": "/stonks/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/stonks/robots.txt"
+  }
+]);
